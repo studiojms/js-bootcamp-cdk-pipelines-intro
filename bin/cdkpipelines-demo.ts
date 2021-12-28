65f04638b30ddadce50c3c092bc5f75fbb50c9c7
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2021-12-28 13:24:10 -0300

    Update Github repo and AWS account id

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import { App } from "@aws-cdk/core";
-import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline-stack";
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
 const app = new App();
 
-new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
-  env: { account: "ACCOUNT", region: "us-east-1" },
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '341538094609', region: 'us-east-1' },
 });
 
 app.synth();
--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,9 +1,5 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
-import {
-  CodePipeline,
-  CodePipelineSource,
-  ShellStep,
-} from "@aws-cdk/pipelines";
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 
 /**
  * The stack that defines the application pipeline
@@ -12,17 +8,17 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, "Pipeline", {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       // The pipeline name
-      pipelineName: "MyServicePipeline",
+      pipelineName: 'MyServicePipeline',
 
       // How it will be built and synthesized
-      synth: new ShellStep("Synth", {
+      synth: new ShellStep('Synth', {
         // Where the source can be found
-        input: CodePipelineSource.gitHub("OWNER/REPO", "main"),
+        input: CodePipelineSource.gitHub('studiojms/js-bootcamp-cdk-pipelines-intro', 'main'),
 
         // Install dependencies, build and run cdk synth
-        commands: ["npm ci", "npm run build", "npx cdk synth"],
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
     });
 
